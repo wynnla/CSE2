b9cc0c6419bfd216446a6d59bbb09af9946c78bb
Winston La <wtl218@lehigh.edu>
2015-08-28 15:55:45 +0000

    First Labgit add *git add *git add *!

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Winston La
+//Lab 01
+//HelloWorld
+//
+//  First compile the program HelloWorld.java
+// Run the program HellowWorld.java
+
+//define class
+public class HelloWorld{
+    //provide main method
+    public static void main(String[] args){
+        //print out statement with a box around it
+        System.out.println ("######################");
+        System.out.println ("######################");
+        System.out.println ("#                    #");
+        System.out.println ("# Hello World 2015!!!#");
+        System.out.println ("#                    #");
+        System.out.println ("######################");
+        System.out.println ("######################");
+        System.out.println ("test");
+        //test test
+    }
+}
