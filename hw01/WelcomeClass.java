415307dda1ecc8de5894f002f2d6b83a66a870d8
Winston La <wtl218@lehigh.edu>
2015-08-29 18:22:19 +0000

    uploaded hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////
+//Winston La
+//Lab 01
+//HelloWorld
+//
+//  First compile the program HelloWorld.java
+// Run the program HellowWorld.java
+
+//define class
+public class WelcomeClass{
+    //provide main method
+    public static void main(String[] args){
+        //print out welcome box 
+        System.out.println ("     ------------      ");
+        System.out.println ("     |  Welcome |      ");
+        System.out.println ("     ------------      ");
+        //print out username box with pointy arrows
+        System.out.println ("     ^  ^  ^  ^  ^  ^  ");
+        System.out.println ("    / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println ("   <-w--t--l--2--1--8->");
+        System.out.println ("    \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println ("     v  v  v  v  v  v  ");
+        //print out a boring summary of your summer.
+        System.out.println ("    I spent my summer on Mountaintop");
+        System.out.println ("    I spent three weeks at home. It ");
+        System.out.println ("    was fun. The end.               ");
+        //test test
+    }
+}
